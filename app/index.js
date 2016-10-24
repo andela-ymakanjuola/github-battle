098d54f59210dcfa75c7e9c0631e8eb5960e438f
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2016-10-24 11:17:03 +0100

    create a github profile page using react components and dummy data

--- app/index.js
+++ app/index.js
@@ -1,13 +1,54 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var USER_DATA = {
+  name: 'Yinka Makanjuola',
+  username: 'andela-ymakanjuola',
+  image: 'https://avatars1.githubusercontent.com/u/5328632?v=3&s=140'
+}
+
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{ height:100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return <div> {this.props.name} </div>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
 var HelloWorld = React.createClass({
   render: function () {
-    return ( <div> Hello World </div> )
+    return ( <div> Hello {this.props.name} </div> )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
