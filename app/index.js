f10705ec48ce782a12783aa8476cc26c10066a8c
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2016-10-24 11:43:20 +0100

    create githup profile link using props.children to get username

--- app/index.js
+++ app/index.js
@@ -13,13 +13,26 @@ var ProfilePic = React.createClass({
   }
 });
 
+var Link = React.createClass({
+  changeURL: function () {
+    window.location.replace(this.props.href)
+  },
+  render: function () {
+    return (
+      <span style={{color: 'blue', cursor: 'pointer'}} onClick={this.changeURL}>
+        {this.props.children}
+      </span>
+    )
+  }
+});
+
 var ProfileLink = React.createClass({
   render: function () {
     return (
       <div>
-        <a href={'https://www.github.com/' + this.props.username}>
+        <Link href={'https://www.github.com/' + this.props.username}>
           {this.props.username}
-        </a>
+        </Link>
       </div>
     )
   }
@@ -40,11 +53,6 @@ var Avatar = React.createClass({
         <ProfileLink username={this.props.user.username} />
       </div>
     )
-  }
-});
-var HelloWorld = React.createClass({
-  render: function () {
-    return ( <div> Hello {this.props.name} </div> )
   }
 });
 
