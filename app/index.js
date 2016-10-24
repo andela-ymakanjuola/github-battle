74164ed89d24d5aed5f9b25c569ca5d485afe564
andela-ymakanjuola <yinka.makanjuola@gmail.com>
2016-10-24 13:14:56 +0100

    Setup sentry
    install ravenjs
    store sentry variables from sentry.io project
    configure sentry with raven
    add tags

--- app/index.js
+++ app/index.js
@@ -1,8 +1,23 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes')
+var Raven = require('raven-js');
 
-ReactDOM.render(
-  routes,
-  document.getElementById('app')
-);
+var sentryKey = '2b8e0ceabaeb4e729cbeaa026d822b30';
+var sentryApp = '108742';
+var sentryURL = 'https://' + sentryKey + '@sentry.io/' + sentryApp;
+
+var _APP_INFO = {
+  name: 'Github Battle',
+  branch: 'video4',
+  version: '1.0'
+}
+
+Raven.config(sentryURL, {
+  release: _APP_INFO.version,
+  tags: {
+    branch: _APP_INFO.branch,
+  }
+}).install();
+
+ReactDOM.render(routes, document.getElementById('app') );
